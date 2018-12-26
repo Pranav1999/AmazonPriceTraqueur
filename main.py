5f08c54fd184f4850dd09aa456b5ebe70d8d48af
pranav1999 <pranav0281999@gmail.com>
2018-12-27 01:48:39 +0530

    Added web scraping for proxies, but due to slow response fails sometimes

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
+import random
+
+
+# scrape and get ip proxy
+def get_proxy():
+    ''' IP:port from this website doesn't seem to work '''
+    # url = "https://free-proxy-list.net/"
+    # source = requests.get(url).text
+    # soup = BeautifulSoup(source, "lxml")
+    # proxy_list_table = soup.select("#proxylisttable")
+    # proxy_list_table = proxy_list_table[0]
+    # proxy_list_table_body = proxy_list_table.select("tbody")[0]
+    # proxies = proxy_list_table_body.select("tr")
+    # proxy_dict = dict()
+    # for proxy in proxies:
+    #     proxy_data = proxy.select("td")
+    #     ip = proxy_data[0].text
+    #     port = proxy_data[1].text
+    #     proxy_dict[ip] = port
+    #
+    # random_ip = random.choice(list(proxy_dict.keys()))
+    # proxy = "https://" + random_ip + ":" + proxy_dict.get(random_ip)
+    # print(proxy)
+    # return proxy
+    return "1.20.97.181:55701"
 
 
 # https://www.amazon.in/dp/B01EU2M62S
 def get_source(base_url, asin):
     proxies = {
-        "http": 'http://119.76.140.4:8080',
-        "https": 'http://200.98.166.135:3128'
+        "http": get_proxy(),
+        "https": get_proxy()
     }
 
     user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) ' \
@@ -47,7 +72,6 @@ def get_info(source):
 
 if __name__ == "__main__":
     base_url = "https://www.amazon.in/dp/"
-    # asin_list = ["B01EU2M62S"]
     asin_list = ["B01EU2M62S", "B07DDB1ZN1", "B07JCSNXJ6"]
     for asin in asin_list:
         source = get_source(base_url, asin)
